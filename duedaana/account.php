3ca976cad204f914ead2e7408e0b656e4395645b
Luna (Sukun Li) <sukun.li@qq.com>
2015-06-09 20:47:03 -0400

    layout change for account.php page

--- duedaana/account.php
+++ duedaana/account.php
@@ -13,32 +13,10 @@ $account = new Account($_GET, $_POST);
 		<div id="wrapper">
 			<?php include_once "page/header.txt"; ?>
 			<div id="body">
-				<div class="right-side">
-					<div class="coming-up panel">
-						<?php echo $account->getSide(); ?>
-					</div>
-					<div class="coming-up panel">
-						<h2>Search</h2>
-						<form method="get" action="search.php">
-							<input type="text" size="18" name="search">
-							<input type="submit" value="Search">
-						</form>
-					</div>
-				</div>
-				<div class="content">
-					<div class="panel">
-						<h2>Account</h2><br>
-						<form method="post" action="?action=update">
-							<script>
-								function handleStateOrProvince(value) {
-									var element = document.getElementById("label");
-									if(value=="CANADA")
-										element.innerHTML = "Province:";
-									else
-										element.innerHTML = "State:";
-								}
-							</script>
-							<?php
+				<div class="panel">
+					<h2>Account</h2>
+					<form method="post" action="?action=update">
+						<?php
 							$error = "";
 							if(isset($_GET['error'])) {
 								$error = explode("*", urldecode($_GET['error']));
@@ -47,28 +25,23 @@ $account = new Account($_GET, $_POST);
 								else
 									$error = "";
 							}
-							if($account->doCheck()):
-							?>
-							<div style="float: left;">
-							Class of graduation:<Br><input type="text" name="graduation"><br><br>
-							Email:<br><input type="email" name="email"><br><br>
-							Phone Number:<br><input type="tel" name="phone"><br><br>
-							House Number:<br><input type="number" name="house"><br><br>
-							<input type="submit" value="Update"><br><?php echo $error; ?></div>
-							<div style="float: right; margin-right: 180px;">
-							Password:<br><input type="password" name="password"><br><br>
-							Resident of:<br>
-							<select onchange="handleStateOrProvince(this.value)" name="resident">
-								<option value="USA">USA</option>
-								<option value="CANADA">CANADA</option>
-							</select><br><br>
-							City:<br><input type="text" name="city"><br><br>
-							<label id="label">State:</label><br><input type="text" name="stateorprovince"><br><br></div>
-							<?php else: ?>
+							//if($account->doCheck()):
+						?>
+						<div>
+							<p>Please setup a password and complete your credentials. After submitting, you will be able to log into the website.</p>
+							Username:<br/>
+							<input type="text" name="username"><br><br>
+							Create a spassword:<br>
+							<input type="password" name="password"><br/>
+							<i>At lease eight characters with one uppercase letter, one lowercase letter, and one digit.</i><br><br>
+							Re-enter the password:<br/>
+							<input type="password" name="password"><br><br>
+							<input type="submit" value="Submit"><br><?php echo $error; ?>
+						</div>
+						<?php //else: ?>
 							Sorry, you must be logged in to access this page.
-							<?php endif; ?>
-						</form>
-					</div>
+						<?php //endif; ?>
+					</form>
 				</div>
 			</div>
 		</div>
